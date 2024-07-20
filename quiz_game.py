221aea7b7c647288fa577edc363ce4873d3f653d
Ayush Gupta <90502624+Predator792002@users.noreply.github.com.>
2024-07-20 23:17:34 +0530

    B:Update quizgame.py

---
+++ quiz_game.py
@@ -0,0 +1,11 @@
+#Quiz Game
+
+print("Welcome to Quiz Game!!")
+
+playing = input("Do you want to play? ")
+
+if playing != "yes":
+    print("We are sad to see you go hope we meet again, good day!!")
+    quit()
+
+print("Okay! let's play :)")
