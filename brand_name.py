f087ef429915f31e33f914f2b6b4e8a6e74e86e6
Ayush Gupta <90502624+Predator792002@users.noreply.github.com>
2024-08-03 21:37:42 +0530

    Modified: brand_name.py

---
+++ brand_name.py
@@ -0,0 +1,8 @@
+print("Welcome to the brand name generator program!!!")
+
+city = input("Enter your city name:\n")
+
+pet = input("Enter name of your pet:\n")
+
+print(f"The brand name for your band is {city+ " " + pet}.")
+print("Thank you for visiting.")
